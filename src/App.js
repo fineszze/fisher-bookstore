4a386315f38bbfb61090e6ba6b514ba30dff7065
jeffdong <dong.670@osu.edu>
2020-02-05 18:45:23 -0500

    init the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
 
 export default class App extends Component {
   render() {
-    return <div>Fisher Bookstore</div>;
+    return (
+      <div className="App container">
+        <NavBar bg = "light" expand="lg">
+          <Navbar.Brand>
+            <Link to ="/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+        </NavBar>
+        <Routes />
+      </div>
+    );
   }
 }
+
