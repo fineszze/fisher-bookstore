183557cd9e7a50cc53d3260d5f8b96574a41170a
jeffdong <dong.670@osu.edu>
2020-02-19 00:30:44 -0500

    init the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar } from 'react-bootstrap';
 import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
