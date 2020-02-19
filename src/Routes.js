52bce2a604c76c4ada936875bf1f3fe8515af3f3
jeffdong <dong.670@osu.edu>
2020-02-19 01:16:34 -0500

    init the projcet 5

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./home/Home";
+import Books from "./books/Book";
 
 export default function Routes() {
     return (
---
+++ src/books/Book.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Book.css";
+
+export default function Book() {
+    return (
+        <div className = "Book">
+            <div className = "lander">
+                <i1>Home</i1>
+                <q>Welcome to Fisher Bookstore.</q>
+            </div>
+        </div>
+    );
+}
+
