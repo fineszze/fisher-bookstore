9e6a63ceea20a8657fa701520c6ec040a6abdd48
jeffdong <dong.670@osu.edu>
2020-02-26 01:59:54 -0500

    init the commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import "./App.css";
 import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Routes from "./Routes.js"
 
-export default class App extends Component {
+
+export default class App  extends Component {
   render() {
     return (
       <div className="App container">
