4f9e7fbd21d647ff39453739bf4e607aabac8853
jeffdong <dong.670@osu.edu>
2020-03-15 14:54:55 -0400

    lab 7

--- src/books/Book.js
+++ src/books/Book.js
@@ -1,81 +1,20 @@
-import React, { Component } from "react";
-import "./Book.css";
-import { BookDisplay } from "./BookDisplay";
+import React, { useEffect, useState } from "react";
+import { BookDisplay } from "./BooksDisplay";
 
-export default class Book extends Component {    
-    constructor (props) {
-        super(props);
-        this.state = { 
-            books: [
-                {
-                    id:1,
-                    title: "Domain Driven Design",
-                    author: "Eric Evans",
-                    isbn: "978-0321125217"
-                },
-                {
-                    id: 2,
-                    title: "Accelerate",
-                    author: "Nicole Forsgren",
-                    isbn: "978-1942788331"
-                },
+export default function Books(props) {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        fetch("https://localhost:5001/api/books/")
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
 
-                {
-                    id: 3,
-                    title: "Do Androids Dream of Electric Sheep",
-                    author: "Phillip Dick",
-                    isbn: "978-1942788332"
-                },
-
-                {
-                    id: 4,
-                    title: "I Was Told There'd Be Cake",
-                    author: "Sloane Crosley",
-                    isbn: "978-1942788333"
-                },
-
-                {
-                    id: 5,
-                    title: "A Clockwork Orange",
-                    author: "Anthony Burgess",
-                    isbn: "978-1942788334"
-                },
-
-                {
-                    id: 6,
-                    title: "The Perks of Being a Wallflower",
-                    author: "Stephen Chbosky",
-                    isbn: "978-1942788335"
-                },
-
-                {
-                    id: 7,
-                    title: "The Man Without Qualities",
-                    author: "Robert Musil",
-                    isbn: "978-1942788336"
-                },
-
-                {
-                    id: 8,
-                    title: "The Elephant Tree",
-                    author: "R.D. Ronald",
-                    isbn: "978-1942788337"
-                },
-
-            ]
-        };
-    }    
-    
-    render() {
-        return (
-            <div className = "Books">
-                <div className ="lander">
-                    <BookDisplay books={this.state.books} /> 
-                </div>
+    return (
+        <div className="Books">
+            <div className="lander">
+                <BookDisplay books={data} />
             </div>
-        );
-    }
-
-    
-};
+        </div>
+    );
+}
 
